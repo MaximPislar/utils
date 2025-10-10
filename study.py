6337a475b997606df2a6e33808c3ee33dbd2f0a9
Maksim Pislar <pislarmaxim@gmail.com>
2025-10-10 15:00:17 +0300

    Initial commit: add project files

---
+++ study.py
@@ -0,0 +1,27 @@
+import jwt
+
+# Генерация ключей (обычно делается один раз)
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Создание приватного ключа
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+# Создание публичного ключа
+public_key = private_key.public_key()
+
+# Сохранение ключей
+with open("private.pem", "wb") as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+
+with open("public.pem", "wb") as f:
+    f.write(public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
